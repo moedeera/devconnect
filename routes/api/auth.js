d0609e152c574a91f7d2785576b7baa927d2f5c5
moedeera <82620264+moedeera@users.noreply.github.com>
2021-09-02 08:12:23 -0600

    Update

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,6 +3,11 @@ const router = express.Router();
 const auth = require('../../middleware/auth');
 const User = require('../../models/User');
 
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const config = require ('config')
+const {check, validationResult} = require('express-validator')
+
 
 
 // @route GET API/auth
@@ -31,6 +36,77 @@ res.status(500).send('Server Error')
 
 
 )
+
+
+// @route POST API/auth
+// @desc Authenticate token
+// @access Public
+
+
+router.post('/', async (req,res)=>
+{
+const {email,password} = req.body;
+
+
+if(!email||!password){
+    res.send("Please enter all fields")
+   
+} else {
+try{
+//Check if user exists 
+let user = await User.findOne({email})
+if (!user){
+    return res.status(400).json({errors:[{msg:'User does not exist'}]});
+}
+//check to see if password is match
+const isMatch = await bcrypt.compare(password, user.password);
+// if its not a match
+if (!isMatch){return res.status(400).json({errors:[{msg:'Incorrect Password'}]}) }
+
+
+
+const payload ={
+
+    user:{
+        id:user.id
+    }
+}
+jwt.sign(payload,
+     config.get('jwtSecret'),
+     {expiresIn:360000},
+     (err,token)=>{
+         if(err) throw err;
+         res.json({token})
+     }
+     )
+
+
+res.json(token)
+}
+catch (err){
+
+console.err(err.message)
+res.status(500).send('Server error')
+}
+
+
+    
+}
+
+
+
+
+    
+
+
+
+   
+})
+
+
+
+
+
 
 
 module.exports = router;
