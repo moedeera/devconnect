300b27073cd5dc52096ca7b823ff4da91c56f658
Moe Deera <deeramoe@gmail.com>
2021-09-04 07:46:56 -0600

    Laptop commit 2

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,12 +1,45 @@
 const express = require('express')
 const router = express.Router();
+const auth = require('../../middleware/auth');
+
+const Post = require('../../models/Post');
+const User = require('../../models/User');
+// const checkObjectId = require('../../middleware/checkObjectId');
+
+
+// @route POST API/Posts
+// @desc Create a post
+// @private
+router.post('/', [auth], async (req,res)=>{
+    
+    
+ 
+
+try {
+const user = await User.findById(req.user.id).select('-password');
+const newPost = {
+text:req.body.text,
+name: user.name,
+avatar:user.avatar,
+user:req.user.id
+ 
+} 
+const post = await newPost.save()
+res.json(post)
+
+} catch (err) {
+ 
+ console.error(err.message);
+res.status(500).send('Server Error')    
+
+}
 
 
 
-// @route GET API/Posts
-// @desc Test route
-// @access Public
-router.get('/', (req,res)=>res.send('Posts route'))
+}
+    
+    
+    )
 
 
 module.exports = router;
