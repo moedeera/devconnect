f8410bc7473c599655ffc8133f4f5c0a24ae007e
moedeera <82620264+moedeera@users.noreply.github.com>
2021-10-25 11:45:50 -0600

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, {Fragment} from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Navbar  from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
-import Register from './components/auth/Register';
+import Register from './components/auth/Register22';
 import Login from './components/auth/Login';
 //Redux
 import {Provider} from 'react-redux'
